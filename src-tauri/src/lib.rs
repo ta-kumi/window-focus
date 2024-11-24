f7d662112e48f6b0cc11f75b40a766c20f167ad7
ta-kumi <33344708+ta-kumi@users.noreply.github.com>
2024-11-24 20:15:02 +0900

    tauri,windows: Add prototype.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,43 +1,51 @@
 use tauri::{
-    menu::{Menu, MenuItem},
+    menu::{Menu, MenuItem, CheckMenuItem},
     tray::TrayIconBuilder,
 };
+use std::sync::{Arc, Mutex};
 
-use std::thread;
-use std::time::Duration;
+mod platform;
+use platform::{
+    MainModule,
+    windows::Windows,
+};
 
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let test = MenuItem::with_id(app, "test", "Test", true, None::<&str>)?;
+            let wf_module = Arc::new(Mutex::new(Windows::new()));
+            wf_module.lock().unwrap().initialize();
+
+            let wf_arc = Arc::clone(&wf_module);
+            let enable = CheckMenuItem::with_id(app, "enable", "Enable", true, true, None::<&str>)?;
             let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-
-            let menu = Menu::with_items(app, &[&test, &quit])?;
-
+            let menu = Menu::with_items(app, &[&enable, &quit])?;
             TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
                 .menu_on_left_click(true)
-                .on_menu_event(|app, event| match event.id().as_ref() {
-                    "test" => {
-                        println!("menu item show clicked");
-                    }
-                    "quit" => {
-                        app.exit(0);
-                    }
-                    _ => {
-                        println!("menu item {:?} not handled", event.id());
+                .on_menu_event(move |app, event| {
+                    match event.id().as_ref() {
+                        "enable" => {
+                            if enable.is_checked().unwrap() {
+                                wf_arc.lock().unwrap().focus_on();
+                                let _ = enable.set_checked(true);
+                            }
+                            else {
+                                wf_arc.lock().unwrap().focus_off();
+                                let _ = enable.set_checked(false);
+                            }
+                        }
+                        "quit" => {
+                            wf_arc.lock().unwrap().finialize();
+                            app.exit(0);
+                        }
+                        _ => {
+                            println!("menu item {:?} not handled", event.id());
+                        }
                     }
                 })
                 .build(app)?;
-
-            thread::spawn(|| {
-                loop {
-                    println!("1sec...");
-                    thread::sleep(Duration::from_secs(1));
-                }
-            });
-
             Ok(())
         })
         .plugin(tauri_plugin_shell::init())
