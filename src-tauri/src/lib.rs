a7eac207ce104b4e6ecafa2a2025381ce17b33ee
ta-kumi <33344708+ta-kumi@users.noreply.github.com>
2024-11-24 21:39:31 +0900

    tauri: Support autolaunch.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,10 @@
 use tauri::{
-    menu::{Menu, MenuItem, CheckMenuItem},
+    menu::{Menu, MenuItem, CheckMenuItem, MenuBuilder},
     tray::TrayIconBuilder,
 };
+use tauri_plugin_autostart::MacosLauncher;
+use tauri_plugin_autostart::ManagerExt;
+
 use std::sync::{Arc, Mutex};
 
 mod platform;
@@ -13,13 +16,43 @@ use platform::{
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            let _ = app.handle().plugin(tauri_plugin_autostart::init(
+                MacosLauncher::LaunchAgent,
+                None,
+            ));
+
             let wf_module = Arc::new(Mutex::new(Windows::new()));
             wf_module.lock().unwrap().initialize();
 
             let wf_arc = Arc::clone(&wf_module);
-            let enable = CheckMenuItem::with_id(app, "enable", "Enable", true, true, None::<&str>)?;
-            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&enable, &quit])?;
+            let enable = CheckMenuItem::with_id(
+                app,
+                "enable",
+                "Enable",
+                true,
+                true,
+                None::<&str>
+            )?;
+            let quit = MenuItem::with_id(
+                app,
+                "quit",
+                "Quit",
+                true,
+                None::<&str>
+            )?;
+            let autolaunch = CheckMenuItem::with_id(
+                app,
+                "autolaunch",
+                "AutoLaunch",
+                true,
+                app.autolaunch().is_enabled().unwrap(),
+                None::<&str>
+            )?;
+            let menu = MenuBuilder::new(app.handle())
+                .items(&[&enable, &quit])
+                .separator()
+                .item(&autolaunch)
+                .build()?;
             TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
@@ -39,6 +72,14 @@ pub fn run() {
                         "quit" => {
                             wf_arc.lock().unwrap().finialize();
                             app.exit(0);
+                        }
+                        "autolaunch" => {
+                            if autolaunch.is_checked().unwrap() {
+                                let _ = app.autolaunch().enable();
+                            }
+                            else {
+                                let _ = app.autolaunch().disable();
+                            }
                         }
                         _ => {
                             println!("menu item {:?} not handled", event.id());
